c2dae68705fcaa72e6e9605b5e039d9fcdd236ee
Asmamaw Yehun <chanieasmamaw>
2024-10-20 20:10:20 +0200

    first commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/masterschool/Desktop/digital library WITH FLASK/venv"
+set -gx VIRTUAL_ENV "/Users/masterschool/Desktop/version_control/digital-library-with-flask-creating-DB-/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
